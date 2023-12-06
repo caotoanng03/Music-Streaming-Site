ffcae4da6bca7fae3ff9118950b197e4e9c40863
CaoToan <caotoan940@gmail.com>
2023-12-06 17:10:02 +0100

    Thêm phần preview audio khi tạo bài hát

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -13,3 +13,21 @@ if (uploadImage) {
     });
 }
 // End Upload Image
+
+// Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        if (e.target.files.length) {
+            const audio = URL.createObjectURL(e.target.files[0]);
+
+            source.src = audio;
+            uploadAudioPlay.load();
+        }
+    });
+}
+// End Upload Audio
