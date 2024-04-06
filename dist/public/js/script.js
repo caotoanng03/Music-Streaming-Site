c2a8a86b64b6c74f68eff13896e0ae273f7cb43a
Cao-Toan Nguyen <caotoan940@gmail.com>
2024-04-06 11:42:31 +0200

    Optimized search using lodash-debounce function

--- dist/public/js/script.js
+++ dist/public/js/script.js
@@ -113,37 +113,40 @@ if (boxSearch) {
     const input = boxSearch.querySelector("input[name='keyword']");
     const boxSuggest = boxSearch.querySelector(".inner-suggest");
 
-    input.addEventListener("keyup", () => {
-        const keyword = input.value;
+    input.addEventListener(
+        "input",
+        _.debounce(() => {
+            const keyword = input.value;
 
-        const link = `/search/suggest?keyword=${keyword}`;
+            const link = `/search/suggest?keyword=${keyword}`;
 
-        fetch(link)
-            .then(res => res.json())
-            .then(data => {
-                const songs = data.songs;
+            fetch(link)
+                .then(res => res.json())
+                .then(data => {
+                    const songs = data.songs;
 
-                if (songs.length > 0) {
-                    boxSuggest.classList.add("show");
+                    if (songs.length > 0) {
+                        boxSuggest.classList.add("show");
 
-                    const htmls = songs.map(song => {
-                        return `
-                            <a class="inner-item" href="/songs/detail/${song.slug}">
-                                <div class="inner-image"><img src="${song.avatar}" /></div>
-                                <div class="inner-info">
-                                    <div class="inner-title">${song.title}</div>
-                                    <div class="inner-singer"><i class="fa-solid fa-microphone-lines"></i> ${song.singerInfo.fullName}</div>
-                                </div>
-                            </a>
-                        `;
-                    });
+                        const htmls = songs.map(song => {
+                            return `
+                                <a class="inner-item" href="/songs/detail/${song.slug}">
+                                    <div class="inner-image"><img src="${song.avatar}" /></div>
+                                    <div class="inner-info">
+                                        <div class="inner-title">${song.title}</div>
+                                        <div class="inner-singer"><i class="fa-solid fa-microphone-lines"></i> ${song.singerInfo.fullName}</div>
+                                    </div>
+                                </a>
+                            `;
+                        });
 
-                    const boxList = boxSuggest.querySelector(".inner-list");
-                    boxList.innerHTML = htmls.join("");
-                } else {
-                    boxSuggest.classList.remove("show");
-                }
-            });
-    });
+                        const boxList = boxSuggest.querySelector(".inner-list");
+                        boxList.innerHTML = htmls.join("");
+                    } else {
+                        boxSuggest.classList.remove("show");
+                    }
+                });
+        }, 300)
+    );
 };
 // End Search Suggest
