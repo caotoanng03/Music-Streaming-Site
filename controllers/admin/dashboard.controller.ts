bfb6217cfd8f037f23571728f4bf9a1ab9710862
Cao-Toan Nguyen <caotoan940@gmail.com>
2024-04-14 22:34:36 +0200

    Doashboard page to show stats

--- controllers/admin/dashboard.controller.ts
+++ controllers/admin/dashboard.controller.ts
@@ -1,14 +1,52 @@
 import { Request, Response } from "express";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+import Account from "../../models/account.model";
+import User from "../../models/user.model";
 
 // [GET] /admin/dashboard
 export const index = async (req: Request, res: Response): Promise<void> => {
-    // admin with no authority will get status: bad request
-    // if (res.locals.role.permissions.length <= 0) {
-    //     res.sendStatus(400);
-    //     return;
-    // }
+    const statistic = {
+        song: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        singer: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+    };
+
+    statistic.song.total = await Song.countDocuments({ deleted: false });
+    statistic.song.active = await Song.countDocuments({ status: 'active' });
+    statistic.song.inactive = await Song.countDocuments({ status: 'inactive' });
+
+    statistic.singer.total = await Singer.countDocuments({ deleted: false });
+    statistic.singer.active = await Singer.countDocuments({ status: 'active' });
+    statistic.singer.inactive = await Singer.countDocuments({ status: 'inactive' });
+
+    statistic.account.total = await Account.countDocuments({ deleted: false });
+    statistic.account.active = await Account.countDocuments({ status: 'active' });
+    statistic.account.inactive = await Account.countDocuments({ status: 'inactive' });
+
+    statistic.user.total = await User.countDocuments({ deleted: false });
+    statistic.user.active = await User.countDocuments({ status: 'active' });
+    statistic.user.inactive = await User.countDocuments({ status: 'inactive' });
 
     res.render("admin/pages/dashboard/index", {
-        pageTitle: "Dashboard"
+        pageTitle: "Dashboard",
+        statistic
     });
 };
--- dist/controllers/admin/dashboard.controller.js
+++ dist/controllers/admin/dashboard.controller.js
@@ -8,11 +8,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.index = void 0;
+const song_model_1 = __importDefault(require("../../models/song.model"));
+const singer_model_1 = __importDefault(require("../../models/singer.model"));
+const account_model_1 = __importDefault(require("../../models/account.model"));
+const user_model_1 = __importDefault(require("../../models/user.model"));
 const index = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const statistic = {
+        song: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        singer: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+    };
+    statistic.song.total = yield song_model_1.default.countDocuments({ deleted: false });
+    statistic.song.active = yield song_model_1.default.countDocuments({ status: 'active' });
+    statistic.song.inactive = yield song_model_1.default.countDocuments({ status: 'inactive' });
+    statistic.singer.total = yield singer_model_1.default.countDocuments({ deleted: false });
+    statistic.singer.active = yield singer_model_1.default.countDocuments({ status: 'active' });
+    statistic.singer.inactive = yield singer_model_1.default.countDocuments({ status: 'inactive' });
+    statistic.account.total = yield account_model_1.default.countDocuments({ deleted: false });
+    statistic.account.active = yield account_model_1.default.countDocuments({ status: 'active' });
+    statistic.account.inactive = yield account_model_1.default.countDocuments({ status: 'inactive' });
+    statistic.user.total = yield user_model_1.default.countDocuments({ deleted: false });
+    statistic.user.active = yield user_model_1.default.countDocuments({ status: 'active' });
+    statistic.user.inactive = yield user_model_1.default.countDocuments({ status: 'inactive' });
     res.render("admin/pages/dashboard/index", {
-        pageTitle: "Dashboard"
+        pageTitle: "Dashboard",
+        statistic
     });
 });
 exports.index = index;
